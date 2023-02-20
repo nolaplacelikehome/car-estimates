aff45c22ddc4efbb694d300bf46ad01792260d34
jakedwng <jakedwng@gmail.com>
2023-02-20 14:06:05 -0500

    Add user dto, user signup verification, and user service dependency injection.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+	// Tell DI need user repository
+	constructor(@InjectRepository(User) private repo: Repository<User>) {}
+}
