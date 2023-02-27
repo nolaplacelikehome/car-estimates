8b45186fffa0e18e50eedebf4e349c196680e376
jakedwng <jakedwng@gmail.com>
2023-02-27 14:56:46 -0500

    Some useless controller tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -3,6 +3,8 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
 import { User } from './user.entity';
+import { NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user-dto';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -19,8 +21,9 @@ describe('UsersController', () => {
       },
     }
     testAuthService = {
-      signup: (email: string, password: string) => Promise.resolve({ id: 1, email: 't@t.com', password: 'p' } as User),
-      signin: () => Promise.resolve({ id: 1, email: 't@t.com', password: 'p' } as User)
+      signin: (email: string, password: string) => {
+        return Promise.resolve({ id: 1, email, password } as User)
+      }
     }
 
     const module: TestingModule = await Test.createTestingModule({
@@ -35,5 +38,22 @@ describe('UsersController', () => {
     expect(controller).toBeDefined();
   });
 
+  it('Should return array of users', async () => {
+    const users = await controller.findUsers('t@t.com');
+    expect(users.length).toEqual(1);
+    expect(users[0].email).toEqual('t@t.com')
+  });
 
+  it('Throws an error if user not found', async () => {
+    testUsersService.findOne = () => null;
+    await expect(controller.findUser('1'))
+          .rejects
+          .toThrow(NotFoundException);
+  });
+
+  it('Signing in modifies session and returns user', async () => {
+    const session = { userId: 0 };
+    const user = await controller.signInUser({ email: 't@t.com', password: 'p' }, session)
+    expect(session.userId).toEqual(1);
+  });
 });
