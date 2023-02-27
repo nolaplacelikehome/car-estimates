f86cfb9ad3ee84d67f7903617c0f9969192d2d89
jakedwng <jakedwng@gmail.com>
2023-02-27 15:40:17 -0500

    Add end-to-end authentication test
    Moved validation pipe to app.module.ts.

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,7 @@ async function bootstrap() {
   app.use(cookieSession({
     keys: ['asdf'],
   }));
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true
-  }));
+  
   await app.listen(3000);
 }
 bootstrap();
